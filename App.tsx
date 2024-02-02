224ffb3cf3b0f5790bd14ae1cb0bc5fc1443d204
Julia Crumenauer <julia.crumenauer@gmail.com>
2024-02-02 12:49:27 -0300

    componente de loading

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading'
 
 import theme from './src/theme/dark';
 
@@ -11,7 +12,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ?  <Groups /> : <ActivityIndicator /> } 
+      { !fontsLoaded ?  <Groups /> : <Loading /> } 
     </ThemeProvider>
   );
 }
