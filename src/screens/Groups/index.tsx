a59bb852a03c3b0c3e2864e705603c420de05ea8
Julia Crumenauer <julia.crumenauer@gmail.com>
2024-02-05 18:43:20 -0300

    update

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,79 @@
-import { Container, Title } from './styles';
+import { Image, TouchableOpacity, View } from 'react-native';
+import { ButtonCreateAccount, ButtonLogin, Container, Text, TextInput, Title } from './styles';
+import { CheckBox } from 'react-native-elements';
+import { useState } from 'react';
+
 
 export default function Groups() {
+
+  const imagePath = require('../../assets/ba.png');
+
+  
+    const [isChecked, setIsChecked] = useState(false);
+  
+    const handleCheckboxToggle = () => {
+      setIsChecked(!isChecked);
+    };
+
+    
+
   return (
     <Container>
-      <Title>
-        Groups
-      </Title>
-    </Container>
+
+      <Image source={imagePath} />
+   
+     <Title>
+       Bem vinde ao app BikeAnje! 
+     </Title>
+     <Text>
+       Acesse sua conta de pessoa 
+       voluntária ou aprendiz.
+     </Text>
+
+     <View>
+       
+       <TextInput
+         placeholder="E-mail"
+       />
+
+      <TextInput
+         placeholder="Senha"
+       />
+
+
+   <CheckBox
+   title="Lembrar senha"
+   checked={isChecked}
+   onPress={handleCheckboxToggle}
+   //containerStyle={styles.checkboxContainer}
+   //textStyle={styles.label}
+   />
+
+    
+
+      <ButtonLogin>
+      <TouchableOpacity >
+        <Text>Login</Text>
+      </TouchableOpacity>
+      </ButtonLogin>
+
+      <ButtonCreateAccount>
+      <TouchableOpacity>
+        <Text>Criar Conta</Text>
+      </TouchableOpacity>
+      </ButtonCreateAccount>
+
+     
+       
+     </View>
+
+   
+    
+   
+
+   </Container>
+   
   );
+  
 }
 
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,11 +2,62 @@ import styled from "styled-components/native";
 
 export const Container = styled.View `
     flex: 1;
-    background-color: ${({ theme }) => theme.COLORS.GRAY_600 };
+    background-color: white;
+    //background-color: ${({ theme }) => theme.COLORS.GRAY_600 };
     padding: 24px;
+
+    border: 4px;
+    border-color: orangered;
+    border-radius: 4px;
+
+
 `;
 
 export const Title = styled.Text`
-    color: #fff;
+    color: black;
+    //color: #fff;
     font-size: 32px;
+    align-items: center;
+    text-align: center;
 `;
+
+export const Text = styled.Text`
+    color: gray;
+    align-items: center;
+    text-align: center;
+`
+
+export const TextInput = styled.TextInput`
+    width: 200px;
+    height: 40px;
+    border-color: lightgray;
+    border-width: 1px;
+    border-radius: 8px;
+    padding: 10px;
+    margin-top: 16px;
+    
+`
+
+
+
+export const ButtonLogin = styled.Text`
+    background-color: lightblue;
+    color: white;
+    border-radius: 10px;
+    text-align: center;
+    align-content: center;
+    align-items: center;
+    width: 74px;
+    height: 24px;
+`
+
+export const ButtonCreateAccount = styled.Text`
+    background-color: orange;
+    color: white;
+    border-radius: 10px;
+    text-align: center;
+    align-content: center;
+    align-items: center;
+    width: 74px;
+    height: 24px;
+`
